ac24689033a54344563cc3dd88a80035f66baccb
FouadEAF <fouad.azbi@gmail.com>
2023-09-02 08:24:19 -0700

    a function that multiplies 2 matrices by using the module NumPy

---
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3.5
+""" This module contient a function multiply 2 matrices.
+"""
+import numpy as np
+
+
+def lazy_matrix_mul(m_a, m_b):
+    """ Function that multiplies 2 matrices.
+
+    Args:
+        m_a: matrix a.
+        m_b: matrix b.
+
+    Returns:
+        multiplication of two matrix.
+    """
+
+    return (np.matmul(m_a, m_b))
