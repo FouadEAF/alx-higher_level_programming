8f8ce455b665f77381c560ede88388fdab0dae97
FouadEAF <fouad.azbi@gmail.com>
2023-09-12 14:29:22 -0700

    a function that writes a string to a text file

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,20 +1,19 @@
 #!/usr/bin/python3
-""" This module contain a function that return the nbr of line of a text file
+""" This module contain a function that write to a text file
 """
 
 
-def number_of_lines(filename=""):
-    """ Function that read from a file and print its number of lines
+def write_file(filename="", text=""):
+    """ Function that writes to a text file
 
     Args:
         filename: filename
+        text: text to write
 
     Raises
         Exception: when the file can be opened
 
     """
-    nbr_lines = 0
-    with open(filename, 'r', encoding="utf-8") as f:
-        for line in f:
-            nbr_lines += 1
-    return nbr_lines
+
+    with open(filename, 'w', encoding="utf-8") as f:
+        return f.write(text)
