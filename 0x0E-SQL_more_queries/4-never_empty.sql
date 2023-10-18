94c8eaf0d811f58282852ad10169eb26635cd8e4
FouadEAF <fouad.azbi@gmail.com>
2023-10-18 14:58:18 -0700

    script that creates the table id_not_null on your MySQL server.

---
+++ 0x0E-SQL_more_queries/4-never_empty.sql
@@ -0,0 +1,6 @@
+-- Script that create a table in MySQL server
+-- Query to create the table in your MySQL server
+CREATE TABLE IF NOT EXISTS id_not_null (
+       id INT DEFAULT 1,
+       name VARCHAR(256)
+);
