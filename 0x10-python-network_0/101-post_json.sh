6de704cfec4d0e650cde19381cb8272702a86c0f
FouadEAF <fouad.azbi@gmail.com>
2023-11-30 14:13:10 -0800

    Bash script that sends a JSON POST request to a URL passed as the first argument, and displays the body of the response

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# Script that send a JSON POST request to URL passed as the first argument,
-# and displays the body of the response
+# Script that send a JSON POST request to URL
 curl -s "$1" -d "@$2" -X POST -H "Content-Type: application/json"
